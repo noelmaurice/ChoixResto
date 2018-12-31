95c5add910b936dabe877ceb5c2a4d073ade29cd
NOEL MAURICE <noelmaurice4@gmail.com>
2018-12-31 10:58:59 +0100

    Commit

---
+++ ChoixResto/Models/IDal.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChoixResto.Models
+{
+    public interface IDal : IDisposable
+    {
+        void CreerRestaurant(string nom, string telephone);
+        List<Resto> ObtientTousLesRestaurants();
+    }
+}
